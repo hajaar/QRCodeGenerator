46657e67f8e55a25e046f3f6f8fb0c0cab0ba3fe
Kartik Narayanan <hajaar@icloud.com>
2022-04-25 09:04:49 +0530

    Initial Commit

---
+++ QRCodeGenerator/QRCodeGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  QRCodeGeneratorApp.swift
+//  QRCodeGenerator
+//
+//  Created by Kartik Narayanan on 25/04/22.
+//
+
+import SwiftUI
+
+@main
+struct QRCodeGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
